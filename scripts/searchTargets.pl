1c4e47141d801a05172dc2b6f43c025a56cdb661
CoderWarrior48 <dawson@hamera.com>
2024-08-03 04:44:49 +0100

    Saving before testing install of perl JSON module

--- scripts/searchTargets.pl
+++ scripts/searchTargets.pl
@@ -2,62 +2,29 @@
 
 use strict;
 use warnings;
+use JSON;
+use IPC::System::Simple qw(capture);
 
 # Created by D
 
-#Init code (can't use one-liner because not in bash)
+my $BASE_PATH = $ENV{BASE_PATH};
 
-# Usage statement
-if (@ARGV == 0) {
-    print "Usage: $0 <search>\n";
-    exit 1;
-}
+my $json_data = capture("$BASE_PATH/scripts/queries.py --get_all");
 
-# Config check
-my $scriptPath;
-if (!defined $ENV{CONFIG}) {
-    print "Command executed standalone: Running config\n";
-    my $script_path = `dirname $0`;
-    # Assuming config.sh is a Perl script that needs to be executed
-    do "$script_path/../config/config.pl" or die "Could not run config: $!";
-    $scriptPath = $script_path;
-}
-else {
-    $scriptPath = $ENV{'scriptPath'};
-}
+my $data = decode_json($json_data);
 
-# Var definitions
 my $search_term = $ARGV[0];
 
-# Program
-my $all_data = `$scriptPath/queries.py get all`;
-
-if ($search_term eq "") {
-	printf "$all_data\n";
-}
-else {
-
-	my @targets = split /\n\n/, $all_data;
-	my $is_blank = 0;
-	foreach my $target (@targets) {
-		if ($target =~ /$search_term/)
-		{
-			# Extract the domain and IP
-			my @head = $target =~ /^.*\n/g;
-			# Extract the ports
-			my @ports = $target =~ /Port\s+(\d+):/g;
-			print "$target\n";
-			# Print the results
-			#print "head: @head\n";
-			#my $numPorts = scalar @targets;
-			#my $command = "$scriptPath/print_ports.py $numPorts \033[33m @ports";
-			#my $output = qx($command);
-			#print $output;
-
-		}
-		$is_blank = 1
-	}
-	if ($is_blank == 0) {
-		printf "\033[0;31mNo targets matched\033[0;37m '$search_term'\n"
+foreach my $target (keys %$data) {
+	if(match($target)) {
+		print $target
 	}
 }
+
+sub match {
+	my $target = @_;
+
+	print "Matching: $target"
+
+}
+
